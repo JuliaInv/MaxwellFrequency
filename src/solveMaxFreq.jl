00200ba5d5589b005e73ab7c2831c257fdd0f559
Dave Marchant <dwfmarchant@gmail.com>
2017-03-30 12:31:42 -0700

    Cleaned up solveMaxFreq.  Iterative solvers work again and all tests now pass.

--- src/solveMaxFreq.jl
+++ src/solveMaxFreq.jl
@@ -1,56 +1,70 @@
+
 export solveMaxFreq
 
-function solveMaxFreq(A, rhs, Msig,
-                      M::AbstractMesh,
+"""
+function en, linSolParam = solveMaxFreq(A, rhs, Msig, M, w,linSolParam, flag)
+
+Solves the Maxwell frequency domain forward problem.
+
+inputs: 
+        A::SparseMatrixCSC          - Stiffness matrix
+        rhs::Array                  - Right hand side
+        MsigE::SparseMatrixCSC      - The edge conductivity mass matrix
+        mesh::AbstractMesh          - The mesh
+        w::Real                     - The angular frequency being solved
+        linSolParam::AbstractSolver - Linear solver parameters
+        doTranspose::Int=0          - Solve the transposed system
+       
+       
+       
+output:
+        en:Array{Complex{Float64}}  - Solution (electric fields on cell edges)
+        linSolParam::AbstractSolver - Linear solver parameters
+       
+"""
+function solveMaxFreq(A::SparseMatrixCSC, 
+                      rhs::Union{Array,SparseMatrixCSC}, 
+                      MsigE::SparseMatrixCSC,
+                      mesh::AbstractMesh,
                       w::Real,
                       linSolParam::AbstractSolver,
-                      flag=0)
-	if linSolParam.doClear == 1
-		iterTol = 1e-10
-		mu = 4*pi*1e-7
-		# setup preconditioner using Aphi system
+                      doTranspose::Int=0)
 
-		Grad = getNodalGradientMatrix(M)
-		Ace  = getEdgeAverageMatrix(M)
-		Ane  = abs(Grad)
-		V    = getVolume(M)
-		v    = diag(V)
+    linSolParam.sym = 2 # structurally symmetric
+    en, linSolParam = solveLinearSystem(A,rhs,linSolParam,doTranspose)
 
-		#println(size(Ane'),"   ",size(Ace'),"    ",size(v))
-		muInvCells = Ane'*(Ace'*v)
-		Mmuinvn    = sdiag(muInvCells)
+    return en, linSolParam
+end
 
-		STBa = Grad*Mmuinvn*Grad'
-		Aap = [A + STBa          -(im*w)*Msig*Grad; 
-				 -(im*w)*Grad'*Msig  -(im*w)*Grad'*Msig*Grad];
- 
-		Map(x) = ssor(Aap,x,out=-1)[1]
-
-		P  = [speye(size(A,2)); Grad']
-		MM(x) = P'*Map(P*x)
-		linSolParam.Ainv = MM
-	end
-	en, = solveLinearSystem(A,rhs,linSolParam,flag)
-
-	return en, linSolParam
-end  # function solveMaxFreq
-
-
-
-function solveMaxFreq(A, rhs, Msig,
-                      M::AbstractMesh,
+function solveMaxFreq(A::SparseMatrixCSC, 
+                      rhs::Union{Array,SparseMatrixCSC}, 
+                      MsigE::SparseMatrixCSC,
+                      mesh::AbstractMesh,
                       w::Real,
-                      linSolParam::MUMPSsolver,
-                      flag=0)
-# 
-# 	Solve the maxwell system using MUMPS
-#
-# 	en, = solveMaxFreq(A,rhs,Msig,M::AbstractMesh,w::Real,linSolParam::MUMPSsolver,flag=0)
+                      linSolParam::IterativeSolver,
+                      doTranspose::Int=0)
 
-#
+    # setup preconditioner using Aphi system
+    if linSolParam.doClear == 1
+        mu0 = 4*pi*1e-7
+        MmuN = getNodalMassMatrix(mesh,vec(zeros(mesh.nc).+1/mu0))
+        Grad = getNodalGradientMatrix(mesh)
 
-linSolParam.sym = 2 # structurally symmetric
-en, = solveLinearSystem(A,rhs,linSolParam,flag)
+        STBa = Grad*MmuN*Grad'
+        Aap = [A + STBa          -(im*w)*MsigE*Grad; 
+             -(im*w)*Grad'*MsigE  -(im*w)*Grad'*MsigE*Grad]
 
-return en, linSolParam
-end # function solveMaxFreq
+        Map(x) = ssor(Aap,x,out=-1)[1]
+        P  = [speye(size(A,2)); Grad']
+        MM(x) = P'*Map(P*x)
+
+        linSolParam.Ainv = MM
+        linSolParam.doClear = 0
+    end
+
+    linSolParam.sym = 2 # structurally symmetric
+    en, linSolParam = solveLinearSystem(A,rhs,linSolParam,doTranspose)
+
+    return en, linSolParam    
+
+end
