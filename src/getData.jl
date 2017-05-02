932dfe8b57dcad4b52b17fd85975bbd4e03b1ef8
Dave Marchant <dwfmarchant@gmail.com>
2017-05-02 10:21:59 -0700

    Added getSensMat function for explicitly generating sensitivity matrix.  Removed version of getSensTMatVec that was being used for this previously.

--- src/getData.jl
+++ src/getData.jl
@@ -43,11 +43,34 @@ function getData(sigma::Array{Float64,1},param::MaxwellFreqParamSE,doClear::Bool
 	tempParam = getMaxwellFreqParam(param.Mesh,param.Sources,param.Obs,[],param.freq, param.Ainv)
 	param.Sens=[]
 	D,tempParam = getData(sigma,tempParam)
-	ns = size(param.Sources,2)
-	nr = size(param.Obs,2)	
-	v      = zeros(ns*nr)
-	J    = getSensTMatVec(speye(ns*nr),sigma,tempParam)
+	J = getSensMat(sigma, tempParam)
 	param.Sens = J'
 	clear!(tempParam)	
 	return D, param
+end
+
+function getSensMat(sigma, param)
+    x = eye(size(param.Sources,2)*size(param.Obs,2))
+    U    = param.Fields
+    w    = param.freq
+    P    = param.Obs
+    
+    Ne, = getEdgeConstraints(param.Mesh)
+    Msig = getEdgeMassMatrix(param.Mesh,vec(sigma))
+    Msig = Ne' * Msig * Ne
+    A = getMaxwellFreqMatrix(sigma, param)
+
+    X    = reshape(complex(x),size(P,2),size(U,2),size(x,2))
+    matv = zeros(Complex128,length(sigma),size(x,2))    
+
+    for i=1:size(U,2)
+        Z     = -Ne'*(P*X[:,i,:])
+        Z,    = solveMaxFreq(A,Z,Msig,param.Mesh,w,param.Ainv,1)
+        u     = U[:,i]
+        dAdm  = getdEdgeMassMatrix(param.Mesh,u)
+        dAdm  = -im*w*Ne'*dAdm
+        matv +=  (dAdm'*Z)
+    end
+    
+    return matv
 end
--- src/getSensTMatVec.jl
+++ src/getSensTMatVec.jl
@@ -30,36 +30,6 @@ function getSensTMatVec(x::Vector,sigma::Vector,param::MaxwellFreqParam)
 	return matv
 end
 
-function getSensTMatVec(x::SparseMatrixCSC,sigma::Vector,param::MaxwellFreqParam)
-	# SensT Mat Vec for FV disctretization on OcTree mesh
-	x = full(x)
-	U    = param.Fields
-	w    = param.freq
-	P    = param.Obs
-	
-    Ne, = getEdgeConstraints(param.Mesh)
-    Msig = getEdgeMassMatrix(param.Mesh,vec(sigma))
-    Msig = Ne' * Msig * Ne
-    A = getMaxwellFreqMatrix(sigma, param)
-
-	X    = reshape(complex(x),size(P,2),size(U,2),size(x,2))
-	matv = zeros(Complex128,length(sigma),size(x,2))	
-
-	for i=1:size(U,2)
-		if !all(X.==0)
-			
-			Z     = -Ne'*(P*X[:,i,:])
-			Z,    = solveMaxFreq(A,Z,Msig,param.Mesh,w,param.Ainv,1)
-			u     = U[:,i] 
-			dAdm  = getdEdgeMassMatrix(param.Mesh,u)
-			dAdm  = -im*w*Ne'*dAdm
-			matv +=  (dAdm'*Z)
-		end
-	end
-	
-	return matv
-end
-
 function getSensTMatVec(x::Vector,sigma::Vector,param::MaxwellFreqParamSE)
 	if isempty(param.Sens)
 		warn("getSensTMatVec: Recomputing data to get sensitvity matrix. This should be avoided.")
